4dc8c2d8045a72d9c4dcd15053c92209c3a138fb
Ning Ning Sun <nsun200@live.com>
2017-12-15 23:47:36 -0500

    Start lexing

--- Lox.cs
+++ Lox.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace lox
 {
     class Lox
     {
+        static bool hadError = false;
+
         static void Main(string[] args)
         {
             if (args.Length > 1)
@@ -26,6 +29,11 @@ namespace lox
             try
             {
                 Run(File.ReadAllText(path));
+                
+                if (hadError)
+                {
+                    Environment.Exit(65);
+                }
             }
             catch
             {
@@ -39,18 +47,31 @@ namespace lox
             {
                 Console.Write("> ");
                 Run(Console.ReadLine());
+                hadError = false;
             }
         }
 
         static void Run(string source)
         {
-            //Scanner scanner = new Scanner(source);
-            //List<Token> tokens = scanner.ScanTokens();
+            Scanner scanner = new Scanner(source);
+            List<Token> tokens = scanner.ScanTokens();
 
-            //foreach (Token token in tokens)
-            //{
-            //    Console.WriteLine(token);
-            //}
+            foreach (Token token in tokens)
+            {
+                Console.WriteLine(token);
+            }
         }
+
+        static void Error(int line, string message)
+        {
+            Report(line, "", message);
+        }
+
+        static void Report(int line, string where, string message)
+        {
+            Console.WriteLine("[line " + line + "] Error" + where + ": " + message);
+            hadError = true;
+        }
+
     }
 }
---
+++ Scanner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lox
+{
+    class Scanner
+    {
+        readonly string source;
+        readonly List<Token> tokens = new List<Token>();
+
+        int start = 0;
+        int current = 0;
+        int line = 1;
+
+        public Scanner(string source)
+        {
+            this.source = source;
+        }
+
+        public List<Token> ScanTokens()
+        {
+            while (!IsAtEnd())
+            {
+                start = current;
+            }
+
+            tokens.Add(new Token(TokenType.EOF, "", null, line));
+            return tokens;
+        }
+
+        bool IsAtEnd()
+        {
+            return current >= source.Length;
+        }
+    }
+}
