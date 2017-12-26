e7f9cbe6514a8dc76ccb4ad2653bb9a24c3bf057
Ning Ning Sun <nsun200@live.com>
2017-12-26 13:30:56 -0500

    Start on interpreter

---
+++ lox/Interpreter.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lox
+{
+    class Interpreter : Expr.IVisitor<object>
+    {
+        public object VisitBinaryExpr(Expr.Binary expr)
+        {
+            object left = Evaluate(expr.left);
+            object right = Evaluate(expr.right);
+
+            switch (expr.op.type)
+            {
+                case TokenType.GREATER:
+                    return (double)left > (double)right;
+                case TokenType.GREATER_EQUAL:
+                    return (double)left >= (double)right;
+                case TokenType.LESS:
+                    return (double)left < (double)right;
+                case TokenType.LESS_EQUAL:
+                    return (double)left <= (double)right;
+                case TokenType.MINUS:
+                    return (double)left - (double)right;
+                case TokenType.EQUAL_EQUAL:
+                    return IsEqual(left, right);
+                case TokenType.BANG_EQUAL:
+                    return !IsEqual(left, right);
+                case TokenType.PLUS:
+                    if (left is double && right is double)
+                    {
+                        return (double)left + (double)right;
+                    }
+
+                    if (left is string && right is string)
+                    {
+                        return (string)left + (string)right;
+                    }
+                    break;
+                case TokenType.SLASH:
+                    return (double)left / (double)right;
+                case TokenType.STAR:
+                    return (double)left * (double)right;
+            }
+
+            return null;
+        }
+
+        public object VisitGroupingExpr(Expr.Grouping expr)
+        {
+            return Evaluate(expr.expression);
+        }
+
+        public object VisitLiteralExpr(Expr.Literal expr)
+        {
+            return expr.value;
+        }
+
+        public object VisitUnaryExpr(Expr.Unary expr)
+        {
+            object right = Evaluate(expr.right);
+
+            switch (expr.op.type)
+            {
+                case TokenType.BANG:
+                    return !IsTruthy(right);
+                case TokenType.MINUS:
+                    return -(double)right;
+            }
+
+            return null;
+        }
+
+        bool IsEqual(object a, object b)
+        {
+            if (a == null && b == null)
+            {
+                return true;
+            }
+
+            if (a == null)
+            {
+                return false;
+            }
+
+            return a.Equals(b);
+        }
+
+        bool IsTruthy(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (obj is bool)
+            {
+                return (bool)obj;
+            }
+            return true;
+        }
+
+        object Evaluate(Expr expr)
+        {
+            return expr.Accept(this);
+        }
+    }
+}
